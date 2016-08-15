c1e3b25c554a67e5538f18bc85dc12cb3ccd6def
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2016-08-15 13:27:18 +0200

    stat installation fixes

--- var/spack/repos/builtin/packages/launchmon/package.py
+++ var/spack/repos/builtin/packages/launchmon/package.py
@@ -36,6 +36,9 @@ class Launchmon(Package):
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
     depends_on('libtool', type='build')
+    depends_on('libgcrypt', type='build')
+    depends_on('libelf', type='build')
+    depends_on('boost', type='build')
 
     def install(self, spec, prefix):
         configure(
